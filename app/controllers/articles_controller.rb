7b3c6b0d160e9930ccd114c88d261219b9c84711
Chisaiibeeboo <kate.la@gmail.com>
2014-07-25 21:43:56 -0700

    added delete, edit, update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
@@ -12,10 +13,26 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(
-				 title: params[:article][:title],
-		 		 body: params[:article][:body])
+		@article = Article.new(article_params)
 		@article.save
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:success] = 'Article has been deleted'
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
 		redirect_to article_path(@article)
 	end
 end
